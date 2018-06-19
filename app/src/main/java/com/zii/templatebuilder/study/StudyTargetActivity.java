fc26a526060feb30e1ceb941745a718a4d58eca9
zii <zhangjianjin@iyunmai.com>
2018-06-19 15:29:43 +0800

    1.add study sub sample

--- app/src/main/java/com/zii/templatebuilder/study/StudyTargetActivity.java
+++ app/src/main/java/com/zii/templatebuilder/study/StudyTargetActivity.java
@@ -6,7 +6,7 @@
 //import android.support.v7.app.AppCompatActivity;
 //import android.view.View;
 //import android.widget.TextView;
-//import com.zii.templatebuilder.R;
+//import ${packageName}.R;
 //
 //public class ${activity_name}Activity extends BaseActivity {
 //
